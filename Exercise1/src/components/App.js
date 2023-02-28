12faf4e2ed265eaafa116e0051e0228ee76256e1
Autumn Parker <agmparker@gmail.com>
2023-02-27 18:23:53 -0600

    Initial Commit
    - Initialized the TargetSum app
    - Added background colour styling for the full screen area

---
+++ Exercise1/src/components/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { View, StyleSheet } from 'react-native';
+
+class App extends React.Component {
+    render() {
+        return (
+            <View style={styles.container}>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#ddd',
+        flex: 1,
+
+    },
+})
+
+export default App;
