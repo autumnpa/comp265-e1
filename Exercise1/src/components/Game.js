bf1485791737072215380c222ec0fd8cbfc321f6
Autumn Parker <agmparker@gmail.com>
2023-02-28 11:52:49 -0600

    Selected Number Function
    - Created a function to determine whether a number has been selected or not from the options generated

--- Exercise1/src/components/Game.js
+++ Exercise1/src/components/Game.js
@@ -13,23 +13,34 @@ class Game extends React.Component {
     // ^ Use the unique identifier (position) - key index for every random number
     // Logic - once pressed the number should be tallied when it is equal to or greater than the target sum
     // Use object or array to add selected number status to a collection
+
+    state = {
+      // Everytime a number is pressed it will be pushed to this array
+      selectedNumbers: [0, 4],
+    }
     randomNumbers = Array.from({ length: this.props.randomNumberCount })
       .map(() => 1 + Math.floor(10 * Math.random()))
       target = this.randomNumbers.slice(0, this.props.randomNumberCount - 2)
         .reduce((acc,curr) => acc + curr, 0);
         // TODO: Shuffle the random numbers after testing is completed
-      render() {
-        return (
-          <View style={styles.container}>
-            <Text style={styles.target}>{this.target}</Text>
-            <View style={styles.randomContainer}>
-              {this.randomNumbers.map((randomNumber, index) =>
-                <RandomNumber key={index} number={randomNumber} />
-              )}
-            </View>
-          </View>
-        );
-      }
+     
+      // Function to check if the number is the array is selected
+     isNumberSelected = (numberIndex) => {
+       return this.state.selectedNumbers.indexOf(numberIndex) >= 0;
+     }
+
+     render() {
+       return (
+         <View style={styles.container}>
+           <Text style={styles.target}>{this.target}</Text>
+           <View style={styles.randomContainer}>
+             {this.randomNumbers.map((randomNumber, index) =>
+               <RandomNumber key={index} number={randomNumber} />
+             )}
+           </View>
+         </View>
+       );
+     }
 }
 
 const styles = StyleSheet.create({
