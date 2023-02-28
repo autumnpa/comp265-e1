b159cd8a2d1b24902f0f5638e6143ba6bb2228cd
Autumn Parker <agmparker@gmail.com>
2023-02-28 10:21:52 -0600

    Prop Type
    - Defined the prop type now that I created a new prop called randomNumberCount
    - Imported proptypes package to add the definition

--- Exercise1/src/components/Game.js
+++ Exercise1/src/components/Game.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import { View, Text, StyleSheet } from 'react-native';
 
 class Game extends React.Component {
+    static propTypes = {
+      randomNumberCount: PropTypes.string.isRequired,
+    }
     target = 10 + Math.floor(40 * Math.random());
     render() {
       return (
         <View style={styles.container}>
           <Text style={styles.target}>{this.target}</Text>
-          <Text></Text>
+          <Text>{this.props.randomNumberCount}</Text>
         </View>
       );
     }
